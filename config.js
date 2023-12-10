2af7c5c888e7e46ea403e5983618d840883ef7e5
ramutc <dranzparker@gmail.com>
2023-12-10 13:11:22 +0530

    First commit

---
+++ config.js
@@ -0,0 +1,18 @@
+window._config = {
+
+    cognito: {
+
+        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb         
+
+        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+
+        region: 'us-west-2' // e.g. us-east-2 
+
+    }, 
+
+    api: { 
+
+        invokeUrl: 'https://6ctv4t1016.execute-api.eu-north-1.amazonaws.com/prod'
+    } 
+
+};
